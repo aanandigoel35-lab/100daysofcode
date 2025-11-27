96bbaffdd435ecb4b1861c3afdc9724b6ae10be7
aanandigoel35-lab <aanandigoel35@gmail.com>
2025-11-27 13:13:26 +0530

    Q54.c

---
+++ Q54.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    for(int i=1;i<=4;i++){
+        for(int s=4;s>i;s--) printf(" ");
+        for(int j=1;j<=2*i-1;j++) printf("*");
+        printf("\n");
+    }
+    for(int i=3;i>=1;i--){
+        for(int s=4;s>i;s--) printf(" ");
+        for(int j=1;j<=2*i-1;j++) printf("*");
+        printf("\n");
+    }
+}
