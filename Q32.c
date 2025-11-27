7691c03504db48969ea069f31eb811d1f007bda5
aanandigoel35-lab <aanandigoel35@gmail.com>
2025-11-27 11:57:32 +0530

    Q32.c

---
+++ Q32.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int n,r,rev=0,temp;
+    scanf("%d",&n);
+    temp=n;
+    while(n){r=n%10; rev=rev*10+r; n/=10;}
+    if(temp==rev) printf("Palindrome");
+    else printf("Not Palindrome");
+}
