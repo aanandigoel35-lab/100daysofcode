d47660802d7ea5f485ae842d485a2f774b37254c
aanandigoel35-lab <aanandigoel35@gmail.com>
2025-11-27 11:37:34 +0530

    Q4.c

---
+++ Q4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float radius, area, circumference;
+    float pi = 3.14;
+
+    printf("Enter radius of circle: ");
+    scanf("%f", &radius);
+
+    area = pi * radius * radius;
+    circumference = 2 * pi * radius;
+
+    printf("\nArea of circle = %.2f", area);
+    printf("\nCircumference of circle = %.2f", circumference);
+
+    return 0;
+}
