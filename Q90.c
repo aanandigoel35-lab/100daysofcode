44eed057100c2b13b0c5f8a5b0d492f8dc7af22a
aanandigoel35-lab <aanandigoel35@gmail.com>
2025-11-27 13:30:19 +0530

    Q90.c

---
+++ Q90.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    char s[100];
+    gets(s);
+
+    for(int i=0;s[i];i++){
+        if(s[i]>='A'&&s[i]<='Z') s[i]+=32;
+        else if(s[i]>='a'&&s[i]<='z') s[i]-=32;
+    }
+    puts(s);
+}
