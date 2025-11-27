9af20eaca0edc89e246653a553beed680d79dca4
aanandigoel35-lab <aanandigoel35@gmail.com>
2025-11-27 11:53:27 +0530

    Q24.c

---
+++ Q24.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int u;
+    float bill;
+
+    printf("Enter units: ");
+    scanf("%d",&u);
+
+    if (u <= 100)
+        bill = u * 5;
+    else if (u <= 200)
+        bill = 100*5 + (u-100)*7;
+    else if (u <= 300)
+        bill = 100*5 + 100*7 + (u-200)*10;
+    else
+        bill = 100*5 + 100*7 + 100*10 + (u-300)*12;
+
+    printf("Bill = Rs %.2f", bill);
+    return 0;
+}
